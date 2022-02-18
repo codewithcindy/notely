3133dd46000e33b45e6bb018d611cd65c1e56806
Cindy Taing <cindyhtaing@gmail.com>
2022-02-18 11:32:36 -0800

    Fix port number and db url

--- index.js
+++ index.js
@@ -19,12 +19,8 @@ const flash = require("connect-flash");
 const ejsMate = require("ejs-mate");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-// const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/notelyDB";
-const secret = process.env.SECRET;
-// const port = process.env.PORT || 3000;
-const dbUrl = "mongodb://localhost:27017/notelyDB";
-
-const port = 3000;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/notelyDB";
+const port = process.env.PORT || 3000;
 
 // Mongoose
 mongoose
